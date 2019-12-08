20638e2a5419983678fef80b8e9e15bd87d4cf18
Corentin RAVET <corentin.ravet@eemi.com>
2019-12-08 21:51:30 +0100

    Ajout de l'exercice 3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const url = require("url");
 const hostname = 'localhost';
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
